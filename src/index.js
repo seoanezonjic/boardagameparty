89a93ee1ab123de59bf5899dde0d55d1a795f0c2
seoanezonjic <darklordsone@hotmail.com>
2020-03-22 16:46:19 +0100

    Fix app

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const TicTacToeClient = Client({
   game: TicTacToe,
   board: TicTacToeBoard,
   debug: false,
-  multiplayer: SocketIO({ server: "localhost:8000" })
+  multiplayer: SocketIO({ server: "https://boardgameparty.herokuapp.com/:8000" })
 });
 
 class App extends React.Component {
